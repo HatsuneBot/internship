94f8e703abb6532efab243608b153bbc8cbc8ea9
HatsuneBot <looooki96@gmail.com>
2016-05-25 17:28:33 +0300

    Add project to github

---
+++ task1/task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task1
+{
+    static class Formatting
+    {
+        public static void PrintOut(string s)
+        {
+            int i=0;
+            string[] separators = { ","," ","\n"};
+            string[] words = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            while(i < words.Length)
+            {
+                //Console.WriteLine(words[i]);
+                Console.WriteLine("X:{0} Y:{1}", double.Parse(words[i]), double.Parse(words[i+1]));
+                i+=2;
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char i;
+            string s;
+            
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1. Ввод данных через консоль");
+            Console.WriteLine("2. Загрузка данных из файла");
+            i = char.Parse(Console.ReadLine());
+            switch (i)
+            {
+                case '1':
+                    while (true)
+                    {
+                        s = Console.ReadLine();
+                        if (String.IsNullOrEmpty(s) == true)
+                        { Console.WriteLine("Ну йобана :с"); Console.ReadKey(); break; }
+                        else { Formatting.PrintOut(s); }
+                    }
+                    break;
+                case '2':
+                    Console.WriteLine("Введите имя файла:");
+                    s=Console.ReadLine();
+                    if(File.Exists(s))
+                    {Formatting.PrintOut(File.ReadAllText(s)); Console.ReadKey(); }
+                    else { Console.WriteLine("Ну йобана :с"); Console.ReadKey(); }
+                    break;
+                default:
+                    Console.WriteLine("Ну йобана :с");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
---
+++ task1/task1_WPF/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApplication1
+{
+    
+    /// <summary>
+    /// Приложение форматирует строки по указанному формату
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// При нажатии клавиши "Ок" из текстбоксов считываются значения и передаются в метод PrintOut класса Formatting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            output.AppendText(Formatting.PrintOut(X.GetLineText(0) + "," + Y.GetLineText(0)));
+            X.Clear(); Y.Clear();
+        }
+        /// <summary>
+        /// При нажатии клавиши "Ок" из текстбокса считывается имя файла и содержимое передается в метод PrintOut класса Formatting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            output.AppendText(Formatting.PrintOut(File.ReadAllText(filename.Text)));            
+        }
+
+    }
+    /// <summary>
+    /// Класс Formatting выполняет необходимый функционал
+    /// </summary>
+    static class Formatting
+    {
+        /// <summary>
+        /// Строка result будет содержать в себе результат форматирования
+        /// </summary>
+        public static string result=String.Empty;
+        /// <summary>
+        /// Метод PrintOut форматирует входящий текст и сохраняет его в строке result
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string PrintOut(string s)
+        {
+            result = "";
+            int i = 0;
+            string[] separators = { ",", " ", "\n" };
+            string[] words = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            
+            while (i < words.Length)
+            {
+                //Console.WriteLine(words[i]);
+                result +="X:" + double.Parse(words[i]) + " Y:" + double.Parse(words[i + 1]) + "\n";
+                i += 2;
+            }
+            return result;
+        }
+    }
+}
