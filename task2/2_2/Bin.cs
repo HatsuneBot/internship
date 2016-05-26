087dfb53d78d7812ab52aa70fa3baddb47618943
HatsuneBot <looooki96@gmail.com>
2016-05-26 14:50:02 +0300

    Update task2

---
+++ task2/2_2/Bin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_2
+{
+    public static class Bin
+    {
+        public static string Converter(string i)
+        {
+            return Convert.ToString(int.Parse(i), 2);
+        }
+    }
+}
