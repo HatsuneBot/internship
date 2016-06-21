f0b6082750c66d5a029b5ada2134c0ccdac35362
Наталья Подрез <mikubot@live.com>
2016-06-21 17:15:43 +0300

    Задание 1

--- EncapsulationApp/Task1/Program.cs
+++ EncapsulationApp/Task1/Program.cs
@@ -10,19 +10,24 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            ProgramConverter[] array = {new ProgramConverter(), new ProgramConverter(), new ProgramHelper(), new ProgramHelper()};
-            for (int i=0;i<array.Length;i++)
+
+            ProgramConverter[] array = { new ProgramConverter(), new ProgramConverter(), new ProgramHelper(), new ProgramHelper() };
+
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine("Enter string to convert to C#:");
                 Console.WriteLine(array[i].ConvertToCSharp(Console.ReadLine()));
                 Console.WriteLine("Enter string to convert to Visual Basic:");
                 Console.WriteLine(array[i].ConvertToCSharp(Console.ReadLine()));
-                //Console.WriteLine("Enter a language:");
-                //string lang = Console.ReadLine();
-                //Console.WriteLine("Enter a string to check syntax:");
-                //string input = Console.ReadLine();
-                //Console.WriteLine(array[i].CodeCheckSyntax(input,lang));
+                Console.WriteLine("Enter a language:");
+                string lang = Console.ReadLine();
+                Console.WriteLine("Enter a string to check syntax:");
+                string input = Console.ReadLine();
+                if (array[i] is ProgramHelper)
+                    Console.WriteLine(((ProgramHelper)array[i]).CodeCheckSyntax(input, lang));
+                else Console.WriteLine("Method can not be called");
             }
+            Console.ReadKey();
         }
     }
 }
